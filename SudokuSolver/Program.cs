dc8763ee8d20ef349719bfe590f387ef4f05a33e
Sonja Lindenmaier <s2010307082@fhooe.at>
2022-01-25 10:07:26 +0100

    Console Output added

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -25,6 +25,7 @@ namespace SudokuSolver {
       Console.Write("This Sudoku is ... ");
       bool solved = SudokuSolver.Solve(board);
       Console.WriteLine($"{(solved ? "SOLVED" : "UNSOLVABLE")}");
+      Console.Writelne(board.ToString());
       Console.WriteLine();
       Console.WriteLine("Bye.");
     }
